2b91381e9ed6b9d53bdf776bafb3eb13f979e683
ShutenkoStas <shotenko494@gmail.com>
2026-03-14 23:01:01 +0000

    -Add hiding a folders and unhiding it -Add css class, new eventlestener in js, make changes in leftbar template

--- static/js/flex_leftbar.js
+++ static/js/flex_leftbar.js
@@ -1,11 +1,33 @@
 const leftbar = document.getElementById('leftbar');
 const leftbar_resizer = document.getElementById('leftbar-resizer');
 
+const folder_list = document.getElementById('folder_list');
+const folders_toggle = document.getElementById('folders-toggle');
+const folder_Arrow = document.getElementById('folders-arrow')
+
 let isDragging = false;
 
+let isOpen = true;
 
 const saveWidth = localStorage.getItem('leftbarWidth');
 if (saveWidth) leftbar.style.width = saveWidth;
+
+folders_toggle.addEventListener('click', function() {
+    if (isOpen) {
+        folder_list.style.opacity = '0';
+        setTimeout(function() {
+            folder_list.style.display = 'none';
+        }, 150);
+        folder_Arrow.className = 'bi bi-caret-down-fill';
+    } else {
+        folder_list.style.display = '';
+        setTimeout(function() {
+            folder_list.style.opacity = '1';
+        }, 15);
+        folder_Arrow.className = 'bi bi-caret-up-fill';
+    }
+    isOpen = !isOpen;
+});
 
 leftbar_resizer.addEventListener('mousedown', function(e) {
     isDragging = true;
